59de961e329aa9ba22ca26d84d553805bb2d53dd
Ramesh <kramesh@lowes.com>
2021-07-30 02:54:13 +0530

    LearningPackageApp Initializer

---
+++ letslearn/src/main/java/com/mylearningapp/App.java
@@ -0,0 +1,13 @@
+package com.mylearningapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
